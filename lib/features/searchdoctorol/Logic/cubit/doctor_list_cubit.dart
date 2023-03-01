8d1d7e778f644b5d7ece491d3dec378f6df8862f
diaaahmed01 <thatlostsoul1234@gmail.com>
2023-03-01 20:17:33 +0200

    new

---
+++ lib/features/searchdoctorol/Logic/cubit/doctor_list_cubit.dart
@@ -0,0 +1,36 @@
+import 'package:bloc/bloc.dart';
+import 'package:drlist_bloc_app/features/searchdoctorol/Data/models/review.dart';
+
+import '../../Data/models/model.dart';
+import '../../Data/repos/repostory.dart';
+
+part 'doctor_list_state.dart';
+
+class DoctorListCubit extends Cubit<DoctorListState> {
+  final UserRepository userRepository;
+
+  // final jsonService drjson;
+  List<Usermodel> allDoctors = [];
+  List<Review> allReviews = [];
+  DoctorListCubit(this.userRepository) : super(InitialState());
+
+  List<Usermodel> getAllDoctors() {
+    emit(DoctorListLoading());
+    userRepository.getUser().then((doctors) {
+      emit(DoctorListLoaded(doctors));
+      this.allDoctors = doctors;
+    });
+
+    return allDoctors;
+  }
+
+  List<Review> getReviewById(String doctor) {
+    emit(DoctorReviewLoading());
+    userRepository.getReviewById(doctor).then((reviews) {
+      emit(DoctorReviewLoaded(reviews));
+      this.allReviews = reviews;
+    });
+
+    return allReviews;
+  }
+}
---
+++ lib/features/searchdoctorol/Logic/cubit/doctor_list_state.dart
@@ -0,0 +1,26 @@
+part of 'doctor_list_cubit.dart';
+
+abstract class DoctorListState {}
+
+class InitialState extends DoctorListState {}
+
+class DoctorListLoading extends DoctorListState {}
+
+class DoctorListLoaded extends DoctorListState {
+  final List<Usermodel> doctors;
+
+  DoctorListLoaded(this.doctors);
+}
+
+class DoctorInfoState extends DoctorListState {
+  final Usermodel doctor;
+  DoctorInfoState(this.doctor);
+}
+
+class DoctorReviewLoading extends DoctorListState {}
+
+class DoctorReviewLoaded extends DoctorListState {
+  final List<Review> reviews;
+
+  DoctorReviewLoaded(this.reviews);
+}
